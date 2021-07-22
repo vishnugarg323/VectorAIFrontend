a181502bb35554d152d2b7f0a34f3b1c8c8a9cf6
Vishnu <vishnu.garg@oracle.com>
2021-07-22 22:04:50 +0530

    image drap drop sample created and checked

--- vector-ai/src/App.js
+++ vector-ai/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import ImagePlaceholder from "./image-placeholder";
 
-function App() {
+
+const App = () => {
+  const [dragId, setDragId] = useState();
+  const [images, setimages] = useState([
+    {
+      id: "image-1",
+      color: "red",
+      order: 1
+    },
+    {
+      id: "image-2",
+      color: "green",
+      order: 2
+    },
+    {
+      id: "image-3",
+      color: "blue",
+      order: 3
+    }
+  ]);
+
+  const handleDrag = (ev) => {
+    setDragId(ev.currentTarget.id);
+  };
+
+  const handleDrop = (ev) => {
+    const dragimage = images.find((image) => image.id === dragId);
+    const dropimage = images.find((image) => image.id === ev.currentTarget.id);
+
+    const dragimageOrder = dragimage.order;
+    const dropimageOrder = dropimage.order;
+
+    const newimageState = images.map((image) => {
+      if (image.id === dragId) {
+        image.order = dropimageOrder;
+      }
+      if (image.id === ev.currentTarget.id) {
+        image.order = dragimageOrder;
+      }
+      return image;
+    });
+
+    setimages(newimageState);
+  };
+
+  // const newimageState = images.map((image) => {
+  //   if (image.id === dragId) {
+  //     image.order = dropImageOrder;
+  //   }
+  //   if (image.id === ev.currentTarget.id) {
+  //     image.order = dragImageOrder;
+  //   }
+  //   return image;
+  // });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {images
+        .sort((a, b) => a.order - b.order)
+        .map((image) => (
+          <ImagePlaceholder
+            key={image.id}
+            imageColor={image.color}
+            imageNumber={image.id}
+            handleDrag={handleDrag}
+            handleDrop={handleDrop}
+          />
+        ))}
     </div>
   );
 }
-
 export default App;
